127605a8219256d11d3bfa03f9161bd16cadaf7f
Stian Reistad Røgeberg <srr@dips.no>
2023-12-13 15:22:55 +0100

    Adding multi threading of api calls

---
+++ HandleData/Proxy/ExternalApiProxy.cs
@@ -0,0 +1,26 @@
+﻿using HandleData.Model;
+using System.Text.Json;
+
+namespace HandleData.Proxy
+{
+    public class ExternalApiProxy : IExternalApiProxy
+    {
+        public Task<Data?> GetAsync(string organizationNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<Data?> GetDeserializedData(HttpContent httpContent)
+        {
+            Data? data = new();
+
+            string json = await httpContent.ReadAsStringAsync();
+            if (json != null)
+            {
+                data = JsonSerializer.Deserialize<Data>(json);
+            }
+
+            return data;
+        }
+    }
+}
--- HandleData/Services/DataService.cs
+++ HandleData/Services/DataService.cs
@@ -1,12 +1,63 @@
 ﻿using HandleData.Interfaces;
+using HandleData.Model;
+using HandleData.Proxy;
+using System.Text.Json;
 
 namespace HandleData.Services
 {
-    public class DataService : IDataService
+    public class DataService
     {
-        public Task UpdateFileWithNewData()
+        private IFileHandler _fileHandler;
+        private readonly IExternalApiProxy _externalApiProxy;
+
+        private const int ChunkSize = 100;
+
+        public DataService(IFileHandler fileHandler, IExternalApiProxy externalApiProxy)
         {
-            throw new NotImplementedException();
+            _fileHandler = fileHandler;
+            _externalApiProxy = externalApiProxy;
         }
+
+        public async Task<Data?[][]> UpdateFileWithNewData()
+        {
+            var linesFromFile = new List<string>(); // TODO replace with real data from file.
+
+            var totalCalls = linesFromFile.Count;
+
+            List<Task<Data?[]>> chunkTasks = new();
+
+            for (int i = 0; i < totalCalls; i+= ChunkSize)
+            {
+                int remainingCalls = totalCalls - 1;
+                int callsInThisChunk = Math.Min(ChunkSize, remainingCalls);
+
+                chunkTasks.Add(MakeApiCalls(i, callsInThisChunk, linesFromFile));
+            }
+
+            Data?[][] results = await Task.WhenAll(chunkTasks);
+
+            return results;
+        }
+
+        private async Task<Data?[]> MakeApiCalls(int startIndex, int callsInChunk, List<string> linesFromFile)
+        {
+            Data? data = new();
+            Data?[] results = new Data?[callsInChunk];
+
+            for (int i = 0; i < callsInChunk; i++)
+            {
+                var line = linesFromFile[startIndex + i];
+                string[] splittedLine = line.Split(";");
+                string organizationNum = splittedLine[0];
+
+                var response = await _externalApiProxy.GetAsync(organizationNum);
+
+                if (response is not null && response.IsOk)
+                {
+                    results[i] = response;
+                }
+            }
+            return results;
+        }        
     }
 }
