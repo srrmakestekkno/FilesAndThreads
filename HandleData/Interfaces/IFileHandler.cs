cb441c697b8ec064602ae62c0707d15b13f20125
Stian Reistad Røgeberg <srr@dips.no>
2023-12-13 14:53:23 +0100

    Added file handler

---
+++ HandleData/Interfaces/IFileHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace HandleData.Interfaces
+{
+    public interface IFileHandler
+    {
+        /// <summary>
+        /// Reads all data from file and returns a list with semicolon separated lines.
+        /// </summary>
+        /// <param name="path">Path to the fil.</param>
+        /// <returns></returns>
+        Task<List<string>> ReadFile(string path);
+
+        /// <summary>
+        /// Overwrtie the file with the new data.
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
+        /// <param name="lines">Semi colon separated lines</param>
+        /// <returns></returns>
+        Task WriteFile(string path, List<string> lines);
+        
+
+    }
+}
