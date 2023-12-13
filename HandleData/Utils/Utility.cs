8562e82d8e52be85b8b8700eacfaec177c915c79
Stian Reistad Røgeberg <srr@dips.no>
2023-12-13 14:32:33 +0100

    Added calculation of percentage

---
+++ HandleData/Utils/Utility.cs
@@ -0,0 +1,13 @@
+﻿namespace HandleData.Utils
+{
+    public static class Utility
+    {
+        public static decimal CalculatePercentage(int numberOfEmployees, int totalNumberOfEmployees)
+        {
+            if (numberOfEmployees == 0 || totalNumberOfEmployees == 0)
+                return 0;
+
+            return Math.Round((decimal)numberOfEmployees / totalNumberOfEmployees * 100, 2, MidpointRounding.ToZero);
+        }
+    }
+}
