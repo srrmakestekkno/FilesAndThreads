b89a2f15bc0cfe3b909c300b21c27c3001c5a016
Stian Reistad Røgeberg <srr@dips.no>
2023-12-13 14:44:00 +0100

    Added external api proxy

--- HandleData/Proxy/IExternalApiProxy.cs
+++ HandleData/Proxy/IExternalApiProxy.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HandleData.Model;
 
 namespace HandleData.Proxy
 {
-    internal interface IExternalApiProxy
+    public interface IExternalApiProxy
     {
+        Task<Data?> GetAsync(string organizationNumber);
     }
 }
